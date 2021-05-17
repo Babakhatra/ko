83ecf9e4a7058de9ff2b3c9eb469fd85fd1af8db
Babakhatra <84274457+Babakhatra@users.noreply.github.com>
2021-05-17 18:18:19 +0545

    Create ko.py

---
+++ ko.py
@@ -0,0 +1,12 @@
+print("✾●~~~~~~~●●✦WELLCOM TO BABA KHATRA✦●●~~~~~~~●✾")
+print("❍❍❍❍❍~~~~~~~~~❍❍❍✬✥✬❍❍❍~~~~~~~~~❍❍❍❍❍")
+   
+print("  ៚CREATOR : BABA KHATRAツ")
+print("  ៚COUNTRY : NEPAL⚒⚒")
+print("  ៚NEPALI  : HACKER✦✮")
+print("  ៚DESIGNER; RAHUL MISHRA")
+print("  ៚ⓓⓞⓝⓣ ⓒⓞⓟⓨ ⓜⓨ ⓢⓒⓡⓘⓟⓣ")
+ 
+print("❍❍❍❍❍~~~~~~~~~❍❍❍✬✥✬❍❍❍~~~~~~~~~❍❍❍❍❍")
+print("✾●~~~~~~~●●✦WELLCOM TO BABA KHATRA✦●●~~~~~~~●✾")
+print("❍❍❍❍❍~~~~~~~~~❍❍❍✬✥✬❍❍❍~~~~~~~~~❍❍❍❍❍")
